bda53c789ec872bff7b48a5ed3362717d3bdaabc
senkos <senko@mail.ru>
2024-05-16 22:11:07 +0300

    AddBotton - ок

--- AvaloniaAppMVVM_1/ViewModels/UC1ViewModel.cs
+++ AvaloniaAppMVVM_1/ViewModels/UC1ViewModel.cs
@@ -16,25 +16,29 @@ namespace AvaloniaAppMVVM_1.ViewModels
     public class UC1ViewModel: PageViewModelBase
     {
         public ICommand AddCompany { get; }
-        public List<Company> Companies { get; set; }
+        public ICommand RemoveCompany { get; }
+        
+        public ObservableCollection<Company> Companies { get; set; }
+        public ObservableCollection<Company> Companies1 { get; set; }
         private ApplicationDbContext _contextDB { get; }
         private IBaseRepository<Company> _companyRepository {  get; }
-        ICompanyService _companyService {  get; }
+        private ICompanyService _companyService {  get; }
         public UC1ViewModel(List<Company> companyList, ICompanyService companyService, IBaseRepository<Company> companyRepository, ApplicationDbContext contextDB)
         {
             AddCompany = ReactiveCommand.Create(ButtonAddCompany);
+            RemoveCompany = ReactiveCommand.Create(ButtonRemoveCompany);
             _contextDB = contextDB;
             _companyRepository = companyRepository;
             _companyService = companyService;
+            _companyRepository = new CompanyRepository(_contextDB);
+            _companyService = new CompanyService(_companyRepository);
 
             var companies = companyList;
-            Companies = new List<Company>(companies);
+            Companies = new ObservableCollection<Company>(companies);
         }
 
         private async Task ButtonAddCompany()
         {
-            var _companyRepository = new CompanyRepository(_contextDB);
-            var _companyService = new CompanyService(_companyRepository);
             var response = _companyService.GetCompanies();
 
             if (response.Result.StatusCode == AvaloniaDB.Enums.StatusCode.OK)
@@ -42,8 +46,21 @@ namespace AvaloniaAppMVVM_1.ViewModels
                 var company = new Company { Name = "Компания 1", PhoneNumber = "+7-888-888-88-81", Address = "" };
                 await _companyService.Create(company);
                 var companies = _companyService.GetCompanies().Result.Data;
-                Companies = new List<Company>(companies);
-               
+                company = companies[companies.Count - 1];
+                Companies.Add(company);
+            }
+        }
+
+        private void ButtonRemoveCompany()
+        {
+
+            //var response = _companyService.Delete();
+
+            if (response.Result.StatusCode == AvaloniaDB.Enums.StatusCode.OK)
+            {
+
+
+
             }
         }
 
