a46ddc8099776c23e5242bdd96e5ad7972a5749d
senkos <senko@mail.ru>
2024-06-01 20:39:40 +0300

    промежуточная рабочая

---
+++ AvaloniaAppMVVM_1/Views/Visitor/UCAddVisitorView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaAppMVVM_1.Views.Visitor
+{
+    public partial class UCAddVisitorView : UserControl
+    {
+        public UCAddVisitorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
